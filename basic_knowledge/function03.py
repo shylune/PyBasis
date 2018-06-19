6fb320c1e62ae6920c9c8d8b164a579ce7ddaff4
shylune <shylune@sina.com>
2018-06-19 09:01:09 +0800

    some advanced function of python

--- basic_knowledge/function03.py
+++ basic_knowledge/function03.py
@@ -8,6 +8,7 @@ from functools import reduce
     map函数接收两个参数, 第一个是函数, 第二是Iterable; map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回。
     由于结果是一个Iterator，Iterator是惰性序列，因此可以通过list()函数让它把整个序列都计算出来并返回一个list
     注意： Python27 和 Python3x map返回的结果类型有所不同, 此处以Python3为准
+    
 2、 reduce()
     reduce把一个函数f作用在一个序列[x1, x2, x3, ...]上, 这个函数f必须接收两个参数, reduce把结果继续和序列的下一个元素做累积计算
     其效果相当于： reduce(f, [x1, x2, x3, x4]) == f(f(f(x1, x2), x3), x4)
